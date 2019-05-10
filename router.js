a444a87136c873ffcefeffeee3ffb1247df70ea5
adam <bcuz@users.noreply.github.com>
2019-05-10 09:18:37 -0400

    create a post

--- router.js
+++ router.js
@@ -14,4 +14,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+
+  let { title, contents } = req.body;
+
+  if (!title || !contents) {    
+    return res.status(400).json({errorMessage: "Please provide title and contents for the post." });
+  }
+
+  try {
+    const post = await db.insert(req.body);
+    res.status(201).json(post);
+  } catch (error) {    
+    res.status(500).json({
+      error: "There was an error while saving the post to the database",
+    });
+  }
+});
+
 module.exports = router;

21f6b810e45446697c05f61b72ec3578a99063bb
adam <bcuz@users.noreply.github.com>
2019-05-09 23:20:19 -0400

    hello world

---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require('./server.js')
+
+server.listen(5001, () => {
+  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+});
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const server = express();
+
+// const hubsRouter = require(`./hubs/hubs-router.js`)
+
+server.use(express.json());
+// server.use('/api/hubs', hubsRouter)
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+module.exports = server; // Very Important or it wont run!!

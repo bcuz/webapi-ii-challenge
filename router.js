cbf2185522f5c3a49ac97374f6b906a28c29d36a
adam <bcuz@users.noreply.github.com>
2019-05-11 12:32:41 -0400

    delete and return deleted obj

--- router.js
+++ router.js
@@ -49,4 +49,27 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let posts = await db.findById(id)
+
+    if (posts.length > 0) {
+      try {
+        await db.remove(id)   
+  
+        res.status(200).json(posts[0]);
+        
+      } catch (err) {
+          res.status(500).json({ error: "The post could not be removed" })
+      }
+    } else {
+      res.status(404).json({ error: "The post with the specified ID does not exist." })
+    }
+  } catch (err) {
+      res.status(500).json({ error: "The post information could not be retrieved." })
+  }
+})
+
 module.exports = router;
